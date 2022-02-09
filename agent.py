6e7eae32027e78873baa7fddada8f1e12f241375
tombewley <tom.bewley@bristol.ac.uk>
2022-02-09 17:17:52 +0000

    Add epsilon_greedy method to Agent

--- agent.py
+++ agent.py
@@ -6,19 +6,29 @@ class Agent:
     Base agent class.
     """
 
-    def __init__(self, action_space):
-        self.Q, self.pi = {}, {}
-        for state in action_space:
-            self.Q[state], self.pi[state] = {}, {}
-            n = len(action_space[state])
-            for action in action_space[state]:
-                self.Q[state][action] = 0.
-                self.pi[state][action] = 1 / n # Uniform random policy
+    def __init__(self, action_space, epsilon=1.):
+        self.Q = {state: {action: 0. for action in action_space[state]} for state in action_space}
+        self.epsilon = epsilon
+        self.pi = self.epsilon_greedy(self.Q)
+
+    def epsilon_greedy(self, Q):
+        """Derive an epsilon-greedy policy from a given value function."""
+        pi = {}
+        for state in Q:
+            pi[state] = {}
+            epsilon_per_action = self.epsilon / len(Q[state])
+            for action in Q[state]: pi[state][action] = epsilon_per_action
+            pi[state][self.greedy(Q[state])] += (1 - self.epsilon)
+        return pi
+
+    def greedy(_, Q_state):
+        """Return the greedy action from a dictionary of Q values for a given state."""
+        return max(Q_state, key=lambda action: Q_state[action])
 
     def act(self, state):
         """Sample an action from the current policy."""
         return choice(list(self.pi[state].keys()), p=list(self.pi[state].values()))
 
     def learn(self, state, action, reward, next_state, done):
-        """Update the policy using information from the latest transition."""
+        """Not implemented for the base class."""
         pass
---
+++ q_learning_agent.py
@@ -0,0 +1,11 @@
+from agent import Agent
+
+
+class QLearningAgent(Agent):
+    """
+    Agent class implementing the Q-learning algorithm. 
+    """
+
+    def learn(self, state, action, reward, next_state, done):
+        """Update the policy using information from the latest transition."""
+        pass
