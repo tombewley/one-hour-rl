1728d6080d239cfdf971a5f71800faeadcea6744
tombewley <tom.bewley@bristol.ac.uk>
2022-02-09 18:00:16 +0000

    Add 'default policy'

--- run.py
+++ run.py
@@ -4,8 +4,20 @@ from agent import Agent, QLearningAgent
 
 mdp = StudentMDP(verbose=True)
 
-# agent = Agent(mdp.action_space) # Random agent
-agent = QLearningAgent(mdp.action_space, epsilon=0.5, alpha=0.2, gamma=0.9)
+if True:
+    # Default policy shown in images/student-mdp.png
+    agent = Agent(mdp.action_space) 
+    agent.pi = {
+        "Class 1":  {"Study": 0.5, "Go on Facebook": 0.5},
+        "Class 2":  {"Study": 0.8, "Fall asleep": 0.2},
+        "Class 3":  {"Study": 0.6, "Go to the pub": 0.4},
+        "Facebook": {"Keep scrolling": 0.9, "Close Facebook": 0.1},
+        "Pub":      {"Have a pint": 1.},
+        "Pass":     {"Fall asleep": 1.},
+        "Asleep":   {"Stay asleep": 1.}
+    }
+else:
+    agent = QLearningAgent(mdp.action_space, epsilon=0.5, alpha=0.2, gamma=0.9)
 
 while mdp.ep < 100:
     state = mdp.reset()
