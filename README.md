68a182758a47eacfa9c2cf3324f6b6bb0a64d5cc
tombewley <tom.bewley@bristol.ac.uk>
2022-02-09 14:19:05 +0000

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# One-hour Reinforcement Learning
+
+![The student MDP](images/student-mdp.png?raw=true "The student MDP")
---
+++ mdp.py
@@ -0,0 +1,6 @@
+"""
+
+OpenAI Gym-compatible class for the Student MDP from David Silver's "Introduction to Reinforcement Learning" class.
+See images/student-mdp.png for a diagram of this MDP.
+
+"""
