5bafd36e1b4a8052489d08d4746d4a7f7a3bf903
enjeeneer <scottjeen@gmail.com>
2022-02-22 16:37:34 +0000

    fix: dq network changed from conv to linear

--- dqn_components.py
+++ dqn_components.py
@@ -14,39 +14,24 @@ class DeepQNetwork(nn.Module):
         self.checkpoint_dir = chkpt_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
 
-        self.conv1 = nn.Conv2d(input_dims[0], 32, 8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
+        self.input_dims = input_dims
+        self.fc1_dims = 200
+        self.fc2_dims = 200
+        self.n_actions = n_actions
 
-        fc_input_dims = self.calculate_conv_output_dims(input_dims)
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
 
-        self.fc1 = nn.Linear(fc_input_dims, 512)
-        self.fc2 = nn.Linear(512, n_actions)
-
-        self.optimizer = optim.RMSprop(self.parameters(), lr=lr)
-
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def calculate_conv_output_dims(self, input_dims):
-        state = T.zeros(1, *input_dims)
-        dims - self.conv1(state)
-        dims = self.conv2(dims)
-        dims = self.conv3(dims)
-
-        return int(np.prod(dims.size()))
-
     def forward(self, state):
-        conv1 = F.relu(self.conv1(state))
-        conv2 = F.relu(self.conv2(conv1))
-        conv3 = F.relu(self.conv3(conv2))
-
-        # conv3 shape is BS x n_filters x H x W
-        conv_state = conv3.view(conv3.size()[0], -1)
-        # conv_state shape is BS x (n_filters * H * W)
-        flat = F.relu(self.fc1(conv_state))
-        actions = self.fc2(flat)
+        latent = F.relu(self.fc1(state))
+        latent = F.relu(self.fc2(latent))
+        actions = self.fc3(latent)
 
         return actions
 
